64c18afcaad969a738648b55dba59fa6d202fff9
visualstudio <visualstudio@122-PC24>
2019-01-30 12:23:14 -0500

    commit

--- Co723724-week-04/Program.cs
+++ Co723724-week-04/Program.cs
@@ -10,6 +10,66 @@ namespace Co723724_week_04
     {
         static void Main(string[] args)
         {
+            {
+                countryside blue = new countryside();
+                blue.launch();
+            }
+        }
+        class Village
+        {
+            //Node is an ADT
+            // what KIND of DATA do we Need in a Node?
+            public Village nextVillage;
+            public Village previousVillage;
+            public string Villagename;
+            public bool isAstrildeHere = false;
+
+        }
+        class countryside
+        {
+            Village Maple = new Village();
+            Village Toronto = new Village();
+            Village Ajax = new Village();
+            Village First;
+            Village Last;
+            Village Temp;
+
+
+            public void launch()
+            {
+
+                First = Maple;
+                Last = Ajax;
+                Maple.Villagename = "Maple";
+                Maple.nextVillage = Toronto;
+                Maple.previousVillage = null;
+                Toronto.Villagename = "Toronto:";
+                Toronto.nextVillage = Maple;
+                Toronto.previousVillage = Maple;
+                Ajax.Villagename = " Ajax";
+                Ajax.nextVillage = null;
+                Ajax.previousVillage = Toronto;
+
+                Console.WriteLine(this.displayMap());
+
+
+
+
+            }
+            public string displayMap()
+            {
+                string listOfCities = "";
+                // what problem do i need to solve?
+
+                listOfCities = listOfCities + First.Villagename + "-----";
+                Temp = First.nextVillage;
+                listOfCities = listOfCities + Temp.Villagename + "-------";
+
+
+                return listOfCities;
+            }
         }
     }
+
 }
+
